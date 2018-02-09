25b2def07cfa50885cd02aa566944070a5458be1
user <user@users-MacBook-Pro.local>
2018-02-08 16:28:50 -0800

    initial commit. working

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, './static')));
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    var users_array = [
+        {name: "Michael", email: "michael@codingdojo.com"}, 
+        {name: "Jay", email: "jay@codingdojo.com"}, 
+        {name: "Brendan", email: "brendan@codingdojo.com"}, 
+        {name: "Andrew", email: "andrew@codingdojo.com"}
+    ];
+    res.render('index', {users: users_array});
+})
+
+app.listen(8000, function() {
+    console.log('listening on port 8000');
+});
